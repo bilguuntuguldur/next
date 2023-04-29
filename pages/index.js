34e4bb95bea6e0ec0d265967148cbd1e5b012813
Bilguuntuguldur <bilguutuguldur.0217@gmail.com>
2023-04-29 19:27:46 +0800

    react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Alert from "react-bootstrap/Alert"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        My initial blog!!!
+          {[
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+          ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+              This is a {variant} alert—check it out!
+            </Alert>
+          ))}
       </main>
     </>
   )
